3db6dbde940662e8b448b81a425810da0a80355d
1ilsang <user@AD01690255.local>
2023-08-06 19:41:12 +0900

    feat: Add jotai

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-const getNames2 = () => 123;
+import { atom, useAtom } from "jotai";
+
+const counterAtom = atom(0);
 
 const App = () => {
-  const a = getNames2();
-  return <div>Hello, World!</div>;
+  const [counter, setCounter] = useAtom(counterAtom);
+
+  const isNumber1 = (number) => number === 1;
+
+  const handleButtonClick123 = () => {
+    if (isNumber1(counter)) {
+      setCounter(0);
+      return;
+    }
+    setCounter((prev) => prev + 1);
+  };
+
+  return (
+    <button onClick={handleButtonClick123}>Hello, World! {counter}</button>
+  );
 };
 
 export default App;
